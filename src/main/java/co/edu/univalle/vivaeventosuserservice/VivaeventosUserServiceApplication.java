cedb4deffa370bcd4843f159fe42e7e2f0a2dc5f
Vane2005 <vanessaduranmona@gmail.com>
2026-04-16 18:26:53 -0500

    Enviando archivos base

---
+++ src/main/java/co/edu/univalle/vivaeventosuserservice/VivaeventosUserServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.vivaeventosuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaeventosUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivaeventosUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/univalle/vivaeventosuserservice/VivaeventosUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.vivaeventosuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaeventosUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
