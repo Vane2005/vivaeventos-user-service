72f765c5b1fcfd886454bc2e6c6a631d1d1b54ad
SantUx0521 <santiago.useche@correounivalle.edu.co>
2026-04-24 15:32:47 -0500

    añadida logica del login, probado en postman

--- src/main/java/co/edu/univalle/vivaeventosuserservice/infrastructure/security/JwtService.java
+++ src/main/java/co/edu/univalle/vivaeventosuserservice/infrastructure/security/JwtService.java
@@ -2,6 +2,8 @@ package co.edu.univalle.vivaeventosuserservice.infrastructure.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -9,14 +11,14 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private final String SECRET = "secret_key";
+    private final Key key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(key)
                 .compact();
     }
 }
